307470ed7260dc0637c66cf9c9905e2b745a6e52
drmphobwcodes <mp.musengua@gmail.com>
2023-09-10 10:30:13 -0400

    week1React

---
+++ i-heart-react/src/App.js
@@ -0,0 +1,20 @@
+import HeartsLists from './components/HeartsList';
+import Header from './components/Header';
+
+import './App.css';
+
+const message = 'cool cud';
+
+
+function App() {
+  return  
+  (
+    <>
+    <Header />
+    <HeartsLists />
+
+</>
+  );
+}
+
+export default App;
