2bf0ee08fe5d5cd7c4d9bafca01169621c302448
drmphobwcodes <mp.musengua@gmail.com>
2023-09-26 16:56:36 -0400

    Redux campsitesSlice

--- nucampsite/src/app/store.js
+++ nucampsite/src/app/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import { campsitesReducer } from "../features/campsites/campsitesSlice";
 
 export const store = configureStore({
   reducer: {
